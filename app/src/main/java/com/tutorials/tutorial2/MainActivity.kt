f93f0445026dfe8ec4075b3f1985bcfcda2866fc
Zietieflr <lrmcguire93@gmail.com>
2020-08-07 13:23:00 -0600

    Add date parsing and rendering to screen.

--- app/src/main/java/com/tutorials/tutorial2/MainActivity.kt
+++ app/src/main/java/com/tutorials/tutorial2/MainActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
+import java.text.SimpleDateFormat
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
@@ -25,8 +26,16 @@ class MainActivity : AppCompatActivity() {
         val day = myCalendar.get(Calendar.DAY_OF_MONTH)
         DatePickerDialog(
             this, DatePickerDialog.OnDateSetListener {
-                    view, i, i2, i3 ->
+                    view, selectedYear, selectedMonth, selectedDayOfMonth ->
                 Toast.makeText(this, "Date Picker selected", Toast.LENGTH_LONG).show()
+
+                val selectedDate = "$selectedDayOfMonth/${selectedMonth+1}/$selectedYear"
+
+                tvSelectedDate.setText(selectedDate)
+
+                val simpleDate = SimpleDateFormat("dd/MM/yyyy", Locale.ENGLISH)
+
+                val theDate = simpleDate.parse(selectedDate)
             }, year, month, day).show()
     }
 }
